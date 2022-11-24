2430958cb93719c5c4d034772507cbcb345a8202
Damian Banach <damian070799@wp.pl>
2022-11-24 15:03:17 +0100

    done zad2

--- unity_cwiczenia/Assets/Scripts/lab06/zadanie2_lab6.cs
+++ unity_cwiczenia/Assets/Scripts/lab06/zadanie2_lab6.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class zadanie2_lab6 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool open = false;
+    private Vector3 closed;
+    private Vector3 opened;
+
     void Start()
     {
-        
+        closed = transform.position;
+        opened = new Vector3(closed.x - 3f, closed.y, closed.z + 2f);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (open)
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            transform.position = opened;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.position = closed;
+        }
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (open)
+            {
+                open = false;
+            }
+            else
+            {
+                open = true;
+            }
+        }
+    }
+
 }
