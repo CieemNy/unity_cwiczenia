8f992fc97e3d98b2c89f997ab62f109f1a56267d
Damian Banach <damian070799@wp.pl>
2022-11-24 14:48:06 +0100

    created things needed to lab06

--- unity_cwiczenia/Assets/Scripts/lab04/zadanie4.cs
+++ unity_cwiczenia/Assets/Scripts/lab04/zadanie4.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class zadanie4 : MonoBehaviour
 {
+
     public Transform player;
     float RotationX = 0f;
     float RotationY = 0f;
-    public float sensitivity = 350f;
+    public float sensitivity = 200f;
 
     void Start()
     {
@@ -20,11 +21,11 @@ public class zadanie4 : MonoBehaviour
         float mouseYMove = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
         player.Rotate(Vector3.up * mouseXMove);
-
         RotationX += mouseXMove;
         RotationY += mouseYMove;
-
         RotationY = Mathf.Clamp(RotationY, -90f, 90f);
+
         transform.localRotation = Quaternion.Euler(-RotationY, RotationX, 0f);
+
     }
 }
