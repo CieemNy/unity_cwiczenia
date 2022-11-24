f737b1a2a041ffcd98abbf068cf8bfcbe1c2c9af
Damian Banach <damian070799@wp.pl>
2022-11-24 14:41:56 +0100

    udpate

--- unity_cwiczenia/Assets/Scripts/lab04/zadanie4.cs
+++ unity_cwiczenia/Assets/Scripts/lab04/zadanie4.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-zadanie4 : MonoBehaviour
+
+public class zadanie4 : MonoBehaviour
 {
-    // ruch wokó³ osi Y bêdzie wykonywany na obiekcie gracza, wiêc
-    // potrzebna nam referencja do niego (konkretnie jego komponentu Transform)
     public Transform player;
     float RotationX = 0f;
     float RotationY = 0f;
@@ -12,17 +11,14 @@ zadanie4 : MonoBehaviour
 
     void Start()
     {
-        // zablokowanie kursora na œrodku ekranu, oraz ukrycie kursora
         Cursor.lockState = CursorLockMode.Locked;
     }
-    // Update is called once per frame
+
     void Update()
     {
-        // pobieramy wartoœci dla obu osi ruchu myszy
         float mouseXMove = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseYMove = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
-        // wykonujemy rotacjê wokó³ osi Y
         player.Rotate(Vector3.up * mouseXMove);
 
         RotationX += mouseXMove;
